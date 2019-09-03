607eda036a79a60bca3a6a9e5990ff14b7f8325b
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-03 20:21:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    words.each do |word|
+      if @word.split == word.split
+        return word
+      end
+    #word
+  end
+  end
+end
