62b2cc643d7ba64de4f4f8c89dcc2e77d543f715
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-03 20:28:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,6 +7,6 @@ class Anagram
   end
   
   def match(words)
-    words.select { |word| word.split("") == @word.split("")}
+    words.select { |word| word.split.sort == @word.split.sort }
   end 
 end
